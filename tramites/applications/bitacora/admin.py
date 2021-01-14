e4309c685dd7e6f88148622213e41849e515a465
Wilson <weludenaj@gmail.com>
2021-01-13 23:24:27 -0500

    Cambios 13012021

--- tramites/applications/bitacora/admin.py
+++ tramites/applications/bitacora/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from django.contrib.auth.models import Group, User
-
+from datetime import datetime, date, time, timedelta
 
 # Register your models here.
 from .models import (
@@ -27,11 +27,27 @@ class direccionesAdmin(admin.ModelAdmin):
 class pozosAdmin(admin.ModelAdmin):
     list_display=("numero","direccion", "posicion","tipos","observacion")
     search_fields=("numero", "observacion")  
-
+   
+    
 class bitacoraAdmin(admin.ModelAdmin):
-    list_display=("fecha", "empresa", "direccion", "ingreso", "salida", "observacion")
-    search_fields=("fecha", "empresa", "direccion", "ingreso", "salida", "observacion")
+    list_display=("fecha", "empresa", "direccion", "tecnico", "ingreso", "salida", "observacion")
+    search_fields=("fecha", "tecnico", "ingreso", "salida", "observacion")
     date_hierarchy="fecha"
+    
+    # def post_calculo(self, obj):
+    #     entrada = obj.ingreso
+    #     salida = obj.salida
+    #     format_hora="%H : %M: %S"
+    #     print (entrada)
+    #     print (salida)
+    #     then1 = datetime.strptime("07:17:20","%H:%M:%S")
+    #     print('type of date_object =', type(then1))
+    #     #print("type of date_object =", type(entrada))
+    #     #tiempo= salida.Subtract(entrada).TotalHours;
+    #     tiempo = then1.time + then1.time
+    #     return(tiempo)
+    
+    #     #return obj.salida - obj.ingreso
 
 admin.site.site_header = 'Municipio de Loja 2020' 
 admin.site.register(empresas, empresasAdmin)
